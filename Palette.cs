f6c36f6029a786a5839701da602453857f7b060e
Violet CLM <VioletCLM@gmail.com>
2017-06-06 14:30:41 -0700

    Start a PaletteImage control class
    Begin replacing the mess of Panels in the PaletteForm with a single
    PaletteImage control that (eventually) replicates all their
    functionality.

--- Palette.cs
+++ Palette.cs
@@ -1,5 +1,11 @@
 ﻿using System.IO;
 using System.Linq;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System;
+using System.Collections.Generic;
 
 internal class Palette
 {
@@ -18,6 +24,15 @@ internal class Palette
             if (!LEVformat)
                 color[3] = binreader.ReadByte();
         }
+    }
+
+    public static Color Convert(byte[] src)
+    {
+        return Color.FromArgb(src[0], src[1], src[2], src[3]);
+    }
+    public static byte[] Convert(Color src)
+    {
+        return new byte[4] { src.R, src.G, src.B, src.A };
     }
 
     internal void CopyFrom(Palette other)
@@ -61,6 +76,117 @@ internal class Palette
                 if (!Colors[i].SequenceEqual(other.Colors[i]))
                     return false;
             return true;
+        }
+    }
+}
+
+
+namespace MLLE
+{
+    class PaletteImage : PictureBox
+    {
+        public const int PaletteLengthOnEitherDimension = 16;
+        int ColorSize, BorderSize, ColorTotalSize;
+
+        public int NumberOfColorsToSelectAtATime = 1;
+
+        private Palette _Palette;
+        public Palette Palette {
+            get {
+                return _Palette;
+            }
+            set
+            {
+                _Palette = value;
+                Update(Enumerable.Range(0, (int)Palette.PaletteSize).ToArray());
+            }
+        }
+
+
+        private Bitmap ImageImage;
+        //private BitmapData ImageData;
+        //private byte[] Bytes;
+
+        public PaletteImage(int colorSize, int borderSize)
+        {
+            Width = Height = (int)((ColorTotalSize = (ColorSize = colorSize) + (BorderSize = borderSize) * 2) * PaletteLengthOnEitherDimension);
+            ImageImage = new Bitmap(Width, Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            ColorsSelected = new bool[Palette.PaletteSize];
+
+            MouseDown += Clicked;
+            MouseDoubleClick += DoubleClicked;
+        }
+
+        private int getSelectedColor(MouseEventArgs e)
+        {
+            return (e.X / ColorTotalSize) + (e.Y / ColorTotalSize * PaletteLengthOnEitherDimension);
+        }
+        private void Clicked(object sender, MouseEventArgs e)
+        {
+            int colorSelected = getSelectedColor(e);
+            SetSelected(
+                Enumerable.Range(
+                    colorSelected & ~(NumberOfColorsToSelectAtATime - 1),
+                    NumberOfColorsToSelectAtATime
+                ).ToArray(),
+                !ColorsSelected[colorSelected]
+            );
+        }
+        private void DoubleClicked(object sender, MouseEventArgs e)
+        {
+            int colorSelected = getSelectedColor(e);
+            ColorsSelected[colorSelected] = true;
+
+            ColorDialog colorDialog1 = new ColorDialog();
+            colorDialog1.Color = Palette.Convert(Palette[colorSelected]);
+            DialogResult result = colorDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var color = Palette.Convert(colorDialog1.Color);
+                List<int> numbersOfSelectedColors = new List<int>();
+                for (int i = 0; i < Palette.PaletteSize; ++i)
+                    if (ColorsSelected[i])
+                    {
+                        Palette[i] = color;
+                        numbersOfSelectedColors.Add(i);
+                    }
+                if (numbersOfSelectedColors.Count == 1) //only this one
+                    ColorsSelected[colorSelected] = false;
+                Update(numbersOfSelectedColors.ToArray());
+            }
+        }
+
+        public bool[] ColorsSelected { get; }
+        private void Update(int[] colorsToUpdate)
+        {
+            using (Graphics g = Graphics.FromImage(ImageImage))
+            {
+                using (Brush transparent = new SolidBrush(BackColor))
+                    foreach (var colorToUpdate in colorsToUpdate) {
+                        var color = Palette[colorToUpdate];
+                        int left = (colorToUpdate % PaletteLengthOnEitherDimension) * ColorTotalSize;
+                        int top = (colorToUpdate / PaletteLengthOnEitherDimension) * ColorTotalSize;
+                        g.FillRectangle(ColorsSelected[colorToUpdate] ? Brushes.Black : transparent, new Rectangle(left, top, ColorTotalSize, ColorTotalSize)); //border
+                        using (Brush brush = new SolidBrush(Color.FromArgb(color[0], color[1], color[2])))
+                                g.FillRectangle(brush, new Rectangle(left + BorderSize, top + BorderSize, ColorSize, ColorSize)); //fill
+                    }
+                Image = ImageImage;
+            }
+
+            /*ImageData = ImageImage.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.WriteOnly, PixelFormat.Format32bppArgb);
+            Bytes = new byte[ImageData.Height * ImageData.Stride];
+            {
+                System.Drawing.Drawing2D.Rect
+            }
+            Marshal.Copy(Bytes, 0, ImageData.Scan0, Bytes.Length);
+            ImageImage.UnlockBits(ImageData);
+            Image = ImageImage;*/
+        }
+        public void SetSelected(int[] colorsToSelect, bool selecting)
+        {
+            foreach (var colorToSelect in colorsToSelect)
+                ColorsSelected[colorToSelect] = selecting;
+            Update(colorsToSelect);
         }
     }
 }
--- PaletteForm.cs
+++ PaletteForm.cs
@@ -10,7 +10,6 @@ namespace MLLE
         bool PaletteEdited = false;
         Palette DefaultPalette;
         Palette Palette = new Palette();
-        Panel[] panels = new Panel[Palette.PaletteSize];
 
         public PaletteForm()
         {
@@ -19,8 +18,7 @@ namespace MLLE
 
         void SetAllColors()
         {
-            for (int i = 0; i < Palette.PaletteSize; ++i)
-                panels[i].BackColor = Color.FromArgb(Palette[i][0], Palette[i][1], Palette[i][2]);
+            throw new NotImplementedException();
         }
         void SetColor(Panel panel, Color color)
         {
@@ -32,71 +30,19 @@ namespace MLLE
             PaletteEdited = true;
         }
 
+        PaletteImage PaletteImage = new PaletteImage(15, 2);
         internal Palette ShowForm(Palette plusPalette, Palette defaultPalette)
         {
             DefaultPalette = defaultPalette;
             Palette.CopyFrom(plusPalette ?? DefaultPalette);
-            for (int i = 0; i < Palette.PaletteSize; ++i)
-            {
-                Panel panel = panels[i] = new Panel();
-                panel.Location = new Point(12 + (i % 16) * 19, OKButton.Location.Y + (i / 16) * 19);
-                panel.Size = new Size(16, 16);
-                panel.BorderStyle = BorderStyle.None;
-                panel.Tag = i;
-                if (i < 10 || i == 15 || i > 245)
-                    panel.Enabled = false;
-                else
-                {
-                    panel.Click += Panel_Click;
-                    panel.DoubleClick += Panel_DoubleClick;
-                }
-                Controls.Add(panel);
-            }
-            SetAllColors();
+
+            PaletteImage.Location = new Point(12, OKButton.Location.Y);
+            PaletteImage.Palette = Palette;
+            Controls.Add(PaletteImage);
 
             ShowDialog();
 
             return PaletteEdited ? Palette : null;
-        }
-
-        private void Panel_Click(object sender, EventArgs e)
-        {
-            Panel clickedpanel = sender as Panel;
-            var newBorderStyle = clickedpanel.BorderStyle != BorderStyle.None ? BorderStyle.None : BorderStyle.Fixed3D;
-
-            if (select1ToolStripMenuItem.Checked)
-                clickedpanel.BorderStyle = newBorderStyle;
-            else if (select8ToolStripMenuItem.Checked)
-            {
-                int firstOfGroup = (int)clickedpanel.Tag & ~7;
-                for (uint i = 0; i < 8; ++i)
-                    panels[firstOfGroup + i].BorderStyle = newBorderStyle;
-            }
-            else if (select16ToolStripMenuItem.Checked)
-            {
-                int firstOfGroup = (int)clickedpanel.Tag & ~15;
-                for (uint i = 0; i < 16; ++i)
-                    panels[firstOfGroup + i].BorderStyle = newBorderStyle;
-            }
-        }
-        private void Panel_DoubleClick(object sender, EventArgs e)
-        {
-            Panel clickedpanel = sender as Panel;
-            clickedpanel.BorderStyle = BorderStyle.Fixed3D;
-            colorDialog1.Color = clickedpanel.BackColor;
-            DialogResult result = colorDialog1.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                uint numberOfSelectedPanels = 0;
-                foreach (Panel findpanel in panels)
-                    if (findpanel.BorderStyle == BorderStyle.Fixed3D)
-                    {
-                        ++numberOfSelectedPanels;
-                        SetColor(findpanel, colorDialog1.Color);
-                    }
-                if (numberOfSelectedPanels == 1) //only this one
-                    clickedpanel.BorderStyle = BorderStyle.None;
-            }
         }
 
 
@@ -122,34 +68,37 @@ namespace MLLE
         {
             select1ToolStripMenuItem.Checked = true;
             select8ToolStripMenuItem.Checked = select16ToolStripMenuItem.Checked = false;
+            PaletteImage.NumberOfColorsToSelectAtATime = 1;
         }
 
         private void select8ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             select8ToolStripMenuItem.Checked = true;
             select1ToolStripMenuItem.Checked = select16ToolStripMenuItem.Checked = false;
+            PaletteImage.NumberOfColorsToSelectAtATime = 8;
         }
 
         private void select16ToolStripMenuItem_Click(object sender, EventArgs e)
         {
             select16ToolStripMenuItem.Checked = true;
             select1ToolStripMenuItem.Checked = select8ToolStripMenuItem.Checked = false;
+            PaletteImage.NumberOfColorsToSelectAtATime = 16;
         }
 
         private void selectNoneToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            foreach (Panel panel in panels)
-                panel.BorderStyle = BorderStyle.None;
+            throw new NotImplementedException();
         }
 
         void SwapChannels(int a, int b, int c)
         {
-            foreach (Panel panel in panels)
+            throw new NotImplementedException();
+            /*foreach (Panel panel in panels)
                 if (panel.BorderStyle != BorderStyle.None)
                 {
                     var paletteEntry = Palette[(int)panel.Tag];
                     SetColor(panel, Color.FromArgb(paletteEntry[a], paletteEntry[b], paletteEntry[c]));
-                }
+                }*/
         }
 
         private void swapRedGreenToolStripMenuItem_Click(object sender, EventArgs e) { SwapChannels(1, 0, 2);  }
@@ -158,7 +107,8 @@ namespace MLLE
 
         private void gradientToolStripMenuItem_Click(object sender, EventArgs e) //pretty much copied from JJ2+
         {
-            var selectedPanels = Array.FindAll(panels, panel => panel.BorderStyle != BorderStyle.None);
+            throw new NotImplementedException();
+            /*var selectedPanels = Array.FindAll(panels, panel => panel.BorderStyle != BorderStyle.None);
             var length = selectedPanels.Length - 1;
             if (selectedPanels.Length > 2) //long enough to make a gradient from
             {
@@ -167,7 +117,7 @@ namespace MLLE
                 float[] steps = new float[3] { (last.R - first.R) / (float)length, (last.G - first.G) / (float)length, (last.B - first.B) / (float)length };
                 for (int i = 1; i < length; i++)
                     SetColor(selectedPanels[i], Color.FromArgb((byte)(first.R + (steps[0] * i)), (byte)(first.G + (steps[1] * i)), (byte)(first.B + (steps[2] * i))));
-            }
+            }*/
         }
 
         private void LoadButton_Click(object sender, EventArgs e)
