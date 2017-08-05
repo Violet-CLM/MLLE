d21e170ff58911f15d7953983911aff534a4c9e7
Violet CLM <VioletCLM@gmail.com>
2017-08-04 17:26:22 -0700

    Display editable tile image
    The TileImageEditorForm displays an 8x zoomed image of the tile in
    question

---
+++ TileImageEditorForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace MLLE
+{
+    partial class TileImageEditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonCancel = new System.Windows.Forms.Button();
+            this.OKButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ResetButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ButtonCancel
+            // 
+            this.ButtonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.ButtonCancel.Location = new System.Drawing.Point(280, 41);
+            this.ButtonCancel.Name = "ButtonCancel";
+            this.ButtonCancel.Size = new System.Drawing.Size(75, 23);
+            this.ButtonCancel.TabIndex = 7;
+            this.ButtonCancel.Text = "Cancel";
+            this.ButtonCancel.UseVisualStyleBackColor = true;
+            // 
+            // OKButton
+            // 
+            this.OKButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.OKButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.OKButton.Location = new System.Drawing.Point(280, 12);
+            this.OKButton.Name = "OKButton";
+            this.OKButton.Size = new System.Drawing.Size(75, 23);
+            this.OKButton.TabIndex = 6;
+            this.OKButton.Text = "OK";
+            this.OKButton.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ResetButton
+            // 
+            this.ResetButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResetButton.Location = new System.Drawing.Point(280, 246);
+            this.ResetButton.Name = "ResetButton";
+            this.ResetButton.Size = new System.Drawing.Size(75, 23);
+            this.ResetButton.TabIndex = 9;
+            this.ResetButton.Text = "Reset";
+            this.ResetButton.UseVisualStyleBackColor = true;
+            this.ResetButton.Click += new System.EventHandler(this.ResetButton_Click);
+            // 
+            // TileImageEditorForm
+            // 
+            this.AcceptButton = this.OKButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.ButtonCancel;
+            this.ClientSize = new System.Drawing.Size(367, 282);
+            this.Controls.Add(this.ResetButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ButtonCancel);
+            this.Controls.Add(this.OKButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "TileImageEditorForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Edit Tile Image";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonCancel;
+        private System.Windows.Forms.Button OKButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button ResetButton;
+    }
+}
---
+++ TileImageEditorForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MLLE
+{
+    public partial class TileImageEditorForm : Form
+    {
+        public TileImageEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        byte[] Image, OriginalImage;
+        SolidBrush[] Colors = new SolidBrush[Palette.PaletteSize];
+        Bitmap ImageImage;
+
+        private void ResetButton_Click(object sender, EventArgs e)
+        {
+            Image = OriginalImage.Clone() as byte[];
+            DrawImage();
+        }
+
+        void DrawImage()
+        {
+            for (int x = 0; x < 32; ++x)
+                for (int y = 0; y < 32; ++y)
+                    DrawColor(x, y);
+        }
+        void DrawColor(int x, int y)
+        {
+            using (Graphics g = Graphics.FromImage(ImageImage))
+                g.FillRectangle(Colors[Image[x + y * 32]], new Rectangle(x * 8, y * 8, 8, 8));
+            pictureBox1.Image = ImageImage;
+        }
+
+        internal bool ShowForm(ref byte[] image, byte[] originalImage, Palette palette)
+        {
+            if (palette != null)
+            {
+                PaletteImage original = new PaletteImage(5, 0, true, false);
+                original.Palette = palette;
+                original.Location = new Point(OKButton.Location.X, ButtonCancel.Location.Y + (ButtonCancel.Location.Y - OKButton.Location.Y));
+                Controls.Add(original);
+                for (uint i = 0; i < Palette.PaletteSize; ++i)
+                    Colors[i] = new SolidBrush(Palette.Convert(palette.Colors[i]));
+            }
+
+            Image = (image ?? originalImage).Clone() as byte[];
+            OriginalImage = originalImage;
+            ImageImage = new Bitmap(pictureBox1.Width, pictureBox1.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            DrawImage();
+
+            ShowDialog();
+
+            if (false)
+            {
+                image = Image;
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
