32e977e67faad5273f66732320e024ad3ce3129a
Violet CLM <VioletCLM@gmail.com>
2018-01-02 14:04:04 -0800

    refactoring

--- SmartTiles.cs
+++ SmartTiles.cs
@@ -21,53 +21,65 @@ namespace MLLE
             List<Condition> Conditions = new List<Condition>();
         }
         List<Rule> Rules = new List<Rule>();
-    }
-    partial class Mainframe
-    {
-        List<SmartTile> SmartTiles = new List<SmartTile>();
-        private bool CheckForSmartTileFile()
-        {
-            SmartTiles.Clear();
-            if (!J2L.HasTiles)
-                return false;
-            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MLLESet - " + Path.GetFileNameWithoutExtension(J2L.MainTilesetFilename) + ".txt");
-            Debug.WriteLine(filepath);
-            if (File.Exists(filepath))
-            {
-                Dictionary<string, ushort[]> tileGroups = new Dictionary<string, ushort[]>();
-                bool stillInDictionaryMode = true;
 
-                var lines = File.ReadAllLines(filepath);
-                foreach (string line in lines) {
-                    if (line == String.Empty) //section break
+        static ushort[] CreateTileIDList(IEnumerable<string> words, Dictionary<string, ushort[]> groups)
+        {
+            List<ushort> list = new List<ushort>();
+            foreach (var potentialid in words)
+            {
+                ushort id;
+                ushort[] ids;
+                if (ushort.TryParse(potentialid, out id))
+                    list.Add(id);
+                else if (groups.TryGetValue(potentialid, out ids))
+                    list.AddRange(ids);
+                else
+                    Debug.WriteLine("Error with label " + potentialid);
+            }
+            return list.ToArray();
+        }
+        public static bool DefineSmartTiles(string filepath, out SmartTile[] smartTiles)
+        {
+            List<SmartTile> result = new List<SmartTile>();
+            Dictionary<string, ushort[]> tileGroups = new Dictionary<string, ushort[]>();
+            bool stillInDictionaryMode = true;
+
+            var lines = File.ReadAllLines(filepath);
+            foreach (string line in lines)
+            {
+                if (line == String.Empty) //section break
+                {
+                    stillInDictionaryMode = false;
+                }
+                else
+                {
+                    var words = line.Split(' ');
+                    if (stillInDictionaryMode)
                     {
-                        stillInDictionaryMode = false;
+                        tileGroups.Add(words[0], CreateTileIDList(words.Skip(1), tileGroups));
+                        Debug.WriteLine(words[0]);
+                        Debug.WriteLine(tileGroups[words[0]].Length);
                     }
                     else
                     {
-                        var words = line.Split(' ');
-                        if (stillInDictionaryMode)
-                        {
-                            List<ushort> ids = new List<ushort>();
-                            foreach (var potentialid in words.Skip(1))
-                            {
-                                ushort id;
-                                if (ushort.TryParse(potentialid, out id))
-                                    ids.Add(id);
-                                else
-                                    ids.AddRange(tileGroups[potentialid]);
-                            }
-                            tileGroups.Add(words[0], ids.ToArray());
-                            Debug.WriteLine(words[0]);
-                            Debug.WriteLine(ids.Count);
-                        }
-                        else
-                        {
-                        }
                     }
                 }
-                return true;
             }
+            smartTiles = result.ToArray();
+            return true;
+        }
+    }
+    partial class Mainframe
+    {
+        SmartTile[] SmartTiles = new SmartTile[0];
+        private bool CheckForSmartTileFile()
+        {
+            SmartTiles = new SmartTile[0];
+            if (!J2L.HasTiles)
+                return false;
+            string filepath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "MLLESet - " + Path.GetFileNameWithoutExtension(J2L.MainTilesetFilename) + ".txt");
+            if (File.Exists(filepath))
+                return SmartTile.DefineSmartTiles(filepath, out SmartTiles);
             return false;
         }
     }
