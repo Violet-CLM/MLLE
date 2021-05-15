373faba91584536d4d343a5992f9c8c5af70cf61
Violet CLM <violetclm@gmail.com>
2021-05-15 15:27:39 -0700

    Bump version to 2.16.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("2.15")]
-[assembly: AssemblyFileVersion("2.15")]
+[assembly: AssemblyVersion("2.16")]
+[assembly: AssemblyFileVersion("2.16")]
