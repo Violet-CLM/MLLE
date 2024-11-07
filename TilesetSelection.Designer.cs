9100aadd56b1e390dafd04b4baeae0b5fd861b08
VioletCLM <violetclm@gmail.com>
2024-11-07 12:09:52 -0800

    Show basic tileset info
    tooltips, selected tileset, name, number of tiles, palette

--- TilesetSelection.Designer.cs
+++ TilesetSelection.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
             this.buttonOkay = new System.Windows.Forms.Button();
             this.buttonCancel = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -40,18 +48,19 @@
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.AutoScroll = true;
             this.flowLayoutPanel1.BackColor = System.Drawing.SystemColors.Window;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 11);
-            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(9, 33);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(1);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(825, 400);
             this.flowLayoutPanel1.TabIndex = 0;
             // 
             // buttonOkay
             // 
-            this.buttonOkay.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.buttonOkay.Location = new System.Drawing.Point(12, 550);
+            this.buttonOkay.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOkay.Location = new System.Drawing.Point(12, 493);
             this.buttonOkay.Name = "buttonOkay";
-            this.buttonOkay.Size = new System.Drawing.Size(396, 23);
+            this.buttonOkay.Size = new System.Drawing.Size(684, 23);
             this.buttonOkay.TabIndex = 1;
             this.buttonOkay.Text = "OK";
             this.buttonOkay.UseVisualStyleBackColor = true;
@@ -60,12 +69,79 @@
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(414, 550);
+            this.buttonCancel.Location = new System.Drawing.Point(702, 493);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(418, 23);
+            this.buttonCancel.Size = new System.Drawing.Size(130, 23);
             this.buttonCancel.TabIndex = 2;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 5000;
+            this.toolTip1.InitialDelay = 100;
+            this.toolTip1.ReshowDelay = 50;
+            this.toolTip1.ShowAlways = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Show tilesets with this rating or higher:";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(436, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(176, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Show tilesets with these characters:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(204, 6);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(618, 6);
+            this.textBox1.MaxLength = 30;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(214, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 443);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 39);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "info line 1\r\ninfo line 2\r\ninfo line 3";
             // 
             // TilesetSelection
             // 
@@ -73,20 +149,27 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(844, 585);
+            this.ClientSize = new System.Drawing.Size(844, 528);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOkay);
             this.Controls.Add(this.flowLayoutPanel1);
-            this.Margin = new System.Windows.Forms.Padding(1, 1, 1, 1);
+            this.Margin = new System.Windows.Forms.Padding(1);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(740, 424);
+            this.MinimumSize = new System.Drawing.Size(660, 382);
             this.Name = "TilesetSelection";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Select Tileset";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -95,5 +178,11 @@
         private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
         private System.Windows.Forms.Button buttonOkay;
         private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
     }
 }
--- TilesetSelection.cs
+++ TilesetSelection.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Drawing.Imaging;
+using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -14,6 +15,8 @@ namespace MLLE
 {
     public partial class TilesetSelection : Form
     {
+        PaletteImage TilesetPalette = new PaletteImage(3, 0, true, false);
+
         public TilesetSelection()
         {
             InitializeComponent();
@@ -30,7 +33,6 @@ namespace MLLE
 
                 PictureBox pictureBox1 = new PictureBox();
                 pictureBox1.Size = new Size(160, 160);
-                pictureBox1.BackColor = transparentColor;
                 pictureBox1.Location = new Point(10, 10);
 
                 J2TFile tileset = new J2TFile(f.Filename);
@@ -39,14 +41,13 @@ namespace MLLE
                 var image = new Bitmap(pictureBox1.Width, pictureBox1.Height, System.Drawing.Imaging.PixelFormat.Format8bppIndexed);
                 var data = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.WriteOnly, PixelFormat.Format8bppIndexed);
                 byte[] bytes = new byte[data.Height * data.Stride];
-                //Marshal.Copy(data.Scan0, bytes, 0, bytes.Length);
-                uint numberOfTilesToDraw = Math.Min(tileset.TotalNumberOfTiles, 100);
+                uint numberOfTilesToDraw = Math.Min(tileset.TotalNumberOfTiles, 100); //we're drawing a 160x160 area, which is the first 100 tiles, though not all tilesets even have 100 tiles
                 for (uint i = 0; i < numberOfTilesToDraw; ++i)
                 {
                     var tileImage = tileset.Images[tileset.ImageAddress[i]];
                     var xOrg = (i % 10) * 16;
                     var yOrg = i / 10 * 16;
-                    for (uint x = 0; x < 32; x += 2)
+                    for (uint x = 0; x < 32; x += 2) //+= 2 because this is a 0.5 size thumbnail
                         for (uint y = 0; y < 32; y += 2)
                         {
                             bytes[xOrg + x/2 + (yOrg + y/2) * data.Stride] = tileImage[x + y * 32];
@@ -56,14 +57,55 @@ namespace MLLE
                 image.UnlockBits(data);
                 tileset.Palette.Apply(image);
                 pictureBox1.Image = image;
+                //pictureBox1.Enabled = false;
+                string tooltipText = f.Name /*+ " (" + Path.GetFileNameWithoutExtension(f.Filename) + ")"*/;
+                toolTip1.SetToolTip(pictureBox1, tooltipText);
+               // toolTip1.SetToolTip(panel, tooltipText);
                 panel.Controls.Add(pictureBox1);
                 flowLayoutPanel1.Controls.Add(panel);
+
+                pictureBox1.Tag = tileset;
+                pictureBox1.MouseEnter += PictureBox_MouseEnter;
+                pictureBox1.MouseLeave += PictureBox_MouseLeave;
+                pictureBox1.MouseClick += PictureBox1_MouseClick;
 
                 if (++fileID == 12) //debug
                     break;
             }
 
+            TilesetPalette.Location = new Point(buttonCancel.Right - TilesetPalette.Width, buttonCancel.Top - TilesetPalette.Height - 10);
+            TilesetPalette.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            Controls.Add(TilesetPalette);
+
+            label3.Text = ""; //hide until a tileset is chosen
+
             ShowDialog();
+        }
+
+        Panel SelectedPanel;
+        PictureBox SelectedPictureBox;
+        J2TFile SelectedTileset;
+
+        private void PictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (SelectedPanel != null)
+                SelectedPanel.BackColor = SystemColors.Control; //deselect
+            SelectedPictureBox = sender as PictureBox;
+            SelectedTileset = SelectedPictureBox.Tag as J2TFile;
+            SelectedPanel = SelectedPictureBox.Parent as Panel;
+            label3.Text = SelectedTileset.Name + "\n" + SelectedTileset.FilenameOnly + "\n" + SelectedTileset.TileCount.ToString() + " tiles";
+            TilesetPalette.Palette = SelectedTileset.Palette;
+            TilesetPalette.Update(PaletteImage.AllPaletteColors);
+        }
+
+        private void PictureBox_MouseLeave(object sender, EventArgs e)
+        {
+            Panel panel = (sender as Control).Parent as Panel;
+            panel.BackColor = panel == SelectedPanel ? SystemColors.ControlDark : SystemColors.Control;
+        }
+        private static void PictureBox_MouseEnter(object sender, EventArgs e)
+        {
+            (sender as Control).Parent.BackColor = SystemColors.Highlight;
         }
     }
 }
